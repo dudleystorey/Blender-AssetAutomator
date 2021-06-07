fb332aafe680a6979ac66968f16a2b402156879f
Dudley Storey <dudley.storey@gmail.com>
2021-06-07 14:10:18 -0600

    Automates creation of material assets for Blender

---
+++ assetAutomator.py
@@ -0,0 +1,44 @@
+# assetAutomator
+# generate renderballs, apply appended materials, and mark as assets.
+
+import bpy
+
+C = bpy.context
+renderball = bpy.data.objects["Renderball"]
+base = bpy.data.objects["Base"]
+ballX = renderball.location[0] + 2 #moved "offscreen", so it doesn't get in the way of renders
+ballY = renderball.location[1]
+ballZ = renderball.location[2]
+baseX = base.location[0] + 2 #moved "offscreen", so it doesn't get in the way of renders
+baseY = base.location[1]
+baseZ = base.location[2]
+rows = 0
+maxCols = 17
+
+match_str = "AssetAutomater"
+materials = []
+mats = bpy.data.materials
+
+for x in range (0, len(mats)):
+    if match_str not in mats[x].name:
+        materials.append(mats[x])
+
+while (rows < len(materials) / maxCols):
+    cols = 0
+    while (cols < maxCols):
+        new_renderball = renderball.copy()
+        new_base = base.copy()
+        new_renderball.data = renderball.data.copy()
+        new_base.data = base.data.copy()
+        new_renderball.location = (ballX + (cols * 2), ((ballY + -abs(rows * 3))), ballZ)
+        new_base.location = (baseX + (cols * 2), ((baseY + -abs(rows * 3))), baseZ)
+        appliedMaterial = materials[(rows * maxCols) + cols]
+        new_renderball.name = appliedMaterial.name
+        new_renderball.data.materials[0] = appliedMaterial
+        # bpy.ops.outliner.id_operation(appliedMaterial, 'MARK_ASSET')
+        new_renderball.animation_data_clear()
+        new_base.animation_data_clear()
+        C.collection.objects.link(new_renderball)
+        C.collection.objects.link(new_base)
+        cols += 1
+    rows += 1
---
+++ readme.txt
@@ -0,0 +1,14 @@
+# A simple script to automate automating material asset creation and thumbnailing in Blender 3.0
+# DIRECTIONS:
+
+1. In Blender, append materials to "assetAutomatorTemplate.blend"
+
+2. Run "assetAutomator.py" NOTE: execution with a dozen or more appended materials may take a while.
+
+3. Mark materials you want to keep as assets
+
+4. (OPTIONAL): Move the "cameraSet" group along X and Y in units of 2 to render and save thumbnails of your renderball materials.
+
+5. Save the Blender file with an appropriate name (e.g. "metals.blend")
+
+6. In Blender 3.0 Preferences, point to the saved file under Asset Libraries
